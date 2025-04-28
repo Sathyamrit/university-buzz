dd0d241787513a5f98c982ea96adea8d72d81f30
Sathyamrit <sathya.amrit@gmail.com>
2025-04-29 00:38:26 +0530

    Updates files

--- server/server.js
+++ server/server.js
@@ -1,9 +1,7 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDB } from './config/db.js';
-// import Profile from './models/profile.model.js'; //import the Profile model
-// import { profile } from 'console';
-import profileRoutes from './routes/profile.route.js'; //import the profile routes
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import profileRoutes from "./routes/profile.route.js"; //import the profile routes
 
 dotenv.config();
 
@@ -11,40 +9,32 @@ const app = express();
 
 app.use(express.json()); //middleware to parse JSON data from incoming requests
 
+app.use(express.static('public')); 
+
 app.use("/api/profiles", profileRoutes); //use the profileRoutes for all requests to /api/profiles
 
-
-// app.post('/', async (req, res) => {
-//   const profile = req.body; //user will send the profile data in the body of the request
-
-//   if(!profile.name || !profile.email) {
-//     return res.status(400).json({ success:false,  message: 'Name and email are required' });
-//     // return res.status(400).json({ message: 'Profile data is required' });
-//   }
-
-
-app.post('/profile', (req, res) => {
-  res.send('profile is running'); 
-});
-
-// console.log(process.env.MONGO_URI);
-
-// app.listen(5000, () => {
-//   connectDB();
-//   console.log('Server is running on port 5000');
-//   console.log('click http://localhost:5000 to visit the server');
+// app.post("/profile", (req, res) => {
+//   res.send("profile is running");
 // });
+
+// app.get('/', (req, res) => {
+//   res.send('API is running...'); //send a response when the root URL is accessed
+// });
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: "server/views" }); //send the index.html file when the root URL is accessed
+});
 
 const startServer = async () => {
   try {
     await connectDB(); //connect to the database
     app.listen(5000, () => {
-      console.log('Server is running on port 5000');
-      console.log('click http://localhost:5000 to visit the server');
+      console.log("Server is running on port 5000");
+      console.log("click http://localhost:5000 to visit the server");
     });
   } catch (error) {
-    console.error('Error starting server:', error.message);
+    console.error("Error starting server:", error.message);
   }
 };
 
-startServer(); //call the startServer function to start the server and connect to the database
+startServer();
